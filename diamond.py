22aa8eeaba59d40d14123c4bffbadca497f6ed40
Ankit Malpani <ankitmalpani1975@gmail.com>
2022-05-25 15:05:44 +0530

    Add files via upload

---
+++ diamond.py
@@ -0,0 +1,38 @@
+
+"""for i in range(1,6):
+    for j in range(5-i):
+        print(' ', end=' ')
+    for k in range(6-i,6):
+        print(k,end=' ')
+    
+    print()"""
+
+'''
+for i in range(5):
+    for j in range(i):
+        if i != 0:
+            print(' ', end=' ')
+    for k in range(5-i):
+        print('*', end=' ')
+    print()
+'''
+for i in range(1,6):
+    for j in range(5-i, 0, -1):
+        print(' ', end=' ')
+    for k in range(i):
+        print('*', end=' ')
+    for m in range(i-1):
+        if i != 1:
+            print('*', end=' ')
+    print()
+    
+for i in range(1,5):
+    for j in range(i):
+        print(' ', end=' ')
+    for k in range(5-i, 0, -1):
+        print('*', end=' ')
+    for m in range(4-i):
+        print('*', end=' ')
+    print()
+        
+    
